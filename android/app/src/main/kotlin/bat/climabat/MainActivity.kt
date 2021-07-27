ca12e04d9f27f36a2ce2ea3ee146f39e5136eac8
Batoul_AlAni <56093885+batoul-alani@users.noreply.github.com>
2021-07-27 21:45:22 +0300

    Add files via upload

---
+++ android/app/src/main/kotlin/bat/climabat/MainActivity.kt
@@ -0,0 +1,6 @@
+package bat.climabat
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/screens/location_screen.dart
@@ -0,0 +1,119 @@
+import 'package:flutter/material.dart';
+import 'package:climabat/uti/constants.dart';
+import 'package:climabat/services/weather.dart';
+import 'package:climabat/screens/city_screen.dart';
+
+class LocationScreen extends StatefulWidget {
+  LocationScreen({this.locationWeather});
+  final locationWeather;
+  @override
+  _LocationScreenState createState() => _LocationScreenState();
+}
+
+class _LocationScreenState extends State<LocationScreen> {
+  WeatherModel weatherModel=WeatherModel();
+  int temperature; //integer
+  var cityName;
+  var condition; //integer
+  var weatherIcon;
+  var actionIcon;
+  @override
+  void initState() {
+    super.initState();
+    updateUI(widget.locationWeather);
+  }
+
+  void updateUI(dynamic weatherData){
+    if(weatherData==null){
+      temperature=0;
+      weatherIcon='Error';
+      return;
+    }
+    setState(() {
+    condition = weatherData['weather'][0]['id']; //integer
+    cityName = weatherData['name'];
+    temperature = weatherData['main']['temp'].toInt();
+    weatherIcon = weatherModel.getWeatherIcon(condition);
+    actionIcon= weatherModel.getMessage(temperature); //integer
+    });
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('images/location_background.jpg'),
+            fit: BoxFit.cover,
+            colorFilter: ColorFilter.mode(
+                Colors.white.withOpacity(0.8), BlendMode.dstATop),
+          ),
+        ),
+        constraints: BoxConstraints.expand(),
+        child: SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  FlatButton(
+                    onPressed: () async{
+                      var weatherData=await weatherModel.getLocationWeather();
+                      updateUI(weatherData);
+                    },
+                    child: Icon(
+                      Icons.near_me,
+                      size: 50.0,
+                    ),
+                  ),
+                  FlatButton(
+                    onPressed: () async{
+                      var typedNames= await Navigator.push(context, MaterialPageRoute(builder: (context){
+                        return CityScreen();
+                      },),);
+                      if(typedNames!=null){
+                        var weatherData= await weatherModel.getCityWeather(typedNames);
+                        updateUI(weatherData);
+                      }
+                      print(typedNames);
+                    },
+                    child: Icon(
+                      Icons.location_city,
+                      size: 50.0,
+                    ),
+                  ),
+                ],
+              ),
+              Padding(
+                padding: EdgeInsets.only(left: 15.0),
+                child: Row(
+                  children: <Widget>[
+                    Text(
+                      '$temperature°',
+                      style: kTempTextStyle,
+                    ),
+                    Text(
+                      weatherIcon,
+                      style: kConditionTextStyle,
+                    ),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.only(right: 15.0),
+                child: Text(
+                  '${actionIcon}in $cityName',
+                  textAlign: TextAlign.right,
+                  style: kMessageTextStyle,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
